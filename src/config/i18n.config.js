2979300b6ecd688db21bc908b3a18911d3b6c76f
Lauri Junkkari <latenssi@gmail.com>
2018-11-15 10:35:13 +0200

    Improve locale handling

--- src/config/i18n.config.js
+++ src/config/i18n.config.js
@@ -1,27 +1,16 @@
 'use strict'
 
 import i18n from 'i18next'
-// import { reactI18nextModule } from 'react-i18next'
 
-import translationEN from 'locales/en/translation.json'
-import translationFI from 'locales/fi/translation.json'
+import locales from 'locales'
 
-i18n
-  // .use(reactI18nextModule) // passes i18n down to react-i18next
-  .init({
-    resources: {
-      en: {
-        translation: translationEN
-      },
-      fi: {
-        translation: translationFI
-      }
-    },
-    lng: 'en',
-    fallbackLng: 'en',
-    interpolation: {
-      escapeValue: false // react already handles xss
-    }
-  })
+i18n.init({
+  resources: locales,
+  lng: 'en',
+  fallbackLng: 'en',
+  interpolation: {
+    escapeValue: false // react already handles xss
+  }
+})
 
 export default i18n
--- src/utils/format-time.js
+++ src/utils/format-time.js
@@ -1,13 +1,11 @@
 import { format } from 'date-fns'
-import en from 'date-fns/locale/en'
-import fi from 'date-fns/locale/fi'
 
-const locales = { en, fi }
+import locales from 'locales'
 
 export function getFormattedTimestamp (timestamp) {
   return format(new Date(timestamp), 'HH:mm:ss')
 }
 
 export function getFormattedDateString (date, localeKey = 'en') {
-  return format(date, 'dddd MMMM Do YYYY', { locale: locales[localeKey] })
+  return format(date, 'dddd MMMM Do YYYY', { locale: locales[localeKey].dateLocale })
 }
