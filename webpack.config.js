e30a36694b7680564eada160218267a4d6094858
Lauri Junkkari <latenssi@gmail.com>
2019-02-11 14:00:47 +0200

    Fix public path

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ const config = {
   entry: ['babel-polyfill', './src/index.js'],
   output: {
     path: path.join(__dirname, 'dist'),
-    publicPath: '/',
+    publicPath: './',
     filename: '[name].[hash:20].js'
   },
   resolve: {
